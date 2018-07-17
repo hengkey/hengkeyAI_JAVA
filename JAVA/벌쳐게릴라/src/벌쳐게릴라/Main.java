5326fabc4d04e4c0f7180ce89db9c65b36e4d144
hengkey <hengkey2@naver.com>
2018-07-17 22:52:41 +0900

    .

--- JAVA/벌쳐게릴라/src/벌쳐게릴라/Main.java
+++ JAVA/벌쳐게릴라/src/벌쳐게릴라/Main.java
@@ -17,7 +17,7 @@ public class Main {
 			{ 4, 6 }, { 6, 8 }, { 6, 7 }, { 8, 9 }, { 17, 16 }, { 16, 15 }, { 16, 13 }, { 15, 13 }, { 15, 14 },
 			{ 14, 13 }, { 13, 11 }, { 13, 10 }, { 14, 11 }, { 11, 10 }, { 11, 12 }, { 10, 9 }, { 4, 7 }, { 7, 12 },
 			{ 12, 14 }, { 4, 19 }, { 7, 19 }, { 7, 27 }, { 7, 20 }, { 12, 20 }, { 12, 34 }, { 12, 21 }, { 14, 21 },
-			{ 18, 19 }, { 19, 24 }, { 21, 22 }, { 19, 24 }, { 20, 29 }, { 20, 34 }, { 21, 34 }, { 21, 36 }, { 23, 25 },
+			{ 18, 19 }, { 19, 24 }, { 21, 22 }, { 19, 29 }, { 20, 29 }, { 20, 34 }, { 21, 34 }, { 21, 36 }, { 23, 25 },
 			{ 25, 26 }, { 23, 24 }, { 23, 27 }, { 25, 27 }, { 24, 29 }, { 24, 28 }, { 24, 27 }, { 29, 28 }, { 28, 30 },
 			{ 28, 27 }, { 27, 30 }, { 30, 31 }, { 29, 34 }, { 37, 38 }, { 38, 39 }, { 37, 36 }, { 37, 35 }, { 38, 35 },
 			{ 36, 34 }, { 36, 33 }, { 36, 35 }, { 34, 33 }, { 33, 32 }, { 33, 35 }, { 35, 32 }, { 32, 31 } };
@@ -47,27 +47,61 @@ public class Main {
 			}
 			System.out.println("");
 		}
+
+		int prePos = 1, curPos = 1, targetPos = maxGuerillaPosNum;
+
+		while (curPos != targetPos) {
+			updateEnemyRegion();
+			prePos = curPos;
+			list[curPos].get(0).validFlag = false;
+			curPos = getNextPos(curPos, targetPos);
+			System.out
+					.println("(" + curPos + ":" + guerillaPos[curPos - 1][0] + "," + guerillaPos[curPos - 1][1] + ")");
+			if (curPos == prePos) {
+				resetValidFlag();
+				System.out.println("Oh!! My God!!!");
+//				break;
+			}
+		}
 	}
 
-	public static TilePos getNextTilePos(int curPosIndex, int targetPosIndex) {
-		int nextX = 0;
-		int nextY = 0;
+	public static void resetValidFlag() {
+		for (int i = 1; i <= maxGuerillaPosNum; i++) {
+			list[i].get(0).validFlag = true;
+		}
+	}
+	
+	//업데이트 적군 존재 위치
+	public static void updateEnemyRegion() {
+		list[6].get(0).validFlag = false;
+		list[7].get(0).validFlag = false;
+		list[20].get(0).validFlag = false;
+		list[28].get(0).validFlag = false;
+		list[34].get(0).validFlag = false;
+		list[37].get(0).validFlag = false;
+	}
+
+	public static int getNextPos(int curPosIndex, int targetPosIndex) {
+		int nextPosIndex = curPosIndex;
 		int nearNode = 0;
-		int preDistance = 128 * 128 * 2 + 1;
+		int preDistance = (int) Math.pow(128, 2) * 2 + 1;
 		int curDistance = 0;
 
 		for (int i = 0; i < list[curPosIndex].size(); i++) {
 			nearNode = list[curPosIndex].get(i).node;
-			nextX = guerillaPos[nearNode][0] ;
-			nextY = guerillaPos[nearNode][1] ;
-			curDistance = (int) Math.pow(guerillaPos[targetPosIndex][0] - nextX, 2)
-					+ (int) Math.pow(guerillaPos[targetPosIndex][1] - nextY, 2);
+
+			if (list[nearNode].get(0).validFlag == false)
+				continue;
+
+			curDistance = (int) Math.pow(guerillaPos[targetPosIndex - 1][0] - guerillaPos[nearNode - 1][0], 2)
+					+ (int) Math.pow(guerillaPos[targetPosIndex - 1][1] - guerillaPos[nearNode - 1][1], 2);
 			if (curDistance < preDistance) {
-				TilePos nextPos = new TilePos(nextX,nextY);
+				preDistance = curDistance;
+				nextPosIndex = nearNode;
 			}
 		}
 
-		return nextPos;
+		return nextPosIndex;
 	}
 }
 
